b6e9d9dca6a5c6d53dd56d9beece5aa8936a2233
국창훈 <>
2025-04-07 13:08:26 +0900

    feat: add new palette, custom merge method

--- software/raspberrypi_video/Palettes.h
+++ software/raspberrypi_video/Palettes.h
@@ -4,11 +4,20 @@
 extern const int colormap_rainbow[];
 extern const int colormap_grayscale[];
 extern int colormap_ironblack[];
+//개발
+extern int colormap_custom[];
+//
+
+
 extern int get_size_colormap_rainbow();
 extern int get_size_colormap_grayscale();
 extern int get_size_colormap_ironblack();
-//
+
+//개발
+extern int get_size_colormap_custom();
 extern void customizePalette(int sigMin, int sigMax, int rangeMin, int rangeMax);
-extern void customizePalette();
+extern void customizePalette2(int sigMin, int sigMax, int rangeMin, int rangeMax);
 extern void exportColormapToCSV(int* colormap, int size);
+//미구현
+extern void customizePalette();
 #endif
