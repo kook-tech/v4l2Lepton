aad2f2c7b449fb819f94e1f7aa9fe2088cf9b91c
국창훈 <>
2025-10-28 13:45:30 +0900

    fix. spi 통신 안정성을 위한 spiSpeed 조정 20MHz -> 5MHz

--- software/raspberrypi_video/LeptonThread.cpp
+++ software/raspberrypi_video/LeptonThread.cpp
@@ -30,7 +30,7 @@ LeptonThread::LeptonThread() : QThread()
 	myImageHeight = 60;
 
 	//
-	spiSpeed = 20 * 1000 * 1000; // SPI bus speed 20MHz
+	spiSpeed = 5 * 1000 * 1000; // SPI bus speed 20MHz -> 5MHz
 
 	// min/max value for scaling
 	autoRangeMin = true;
